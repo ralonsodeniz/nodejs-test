09e29e45205af399d977f6a300e9184d87b76197
ralonsodeniz <ralonsodeniz@gmail.com>
2020-03-14 20:26:09 +0100

    Added utility endpoints to store and delete both clients and policies at firestore database and their handler functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 const cors = require("cors");
+
 const { db } = require("./helpers/admin");
 
+const {
+  getAllClients,
+  getAllPolicies,
+  deleteAllClients,
+  deleteAllPolicies
+} = require("./handlers/handlers");
+
 app.use(cors());
+
+app.get("/clients/store", getAllClients);
+app.get("/policies/store", getAllPolicies);
+app.delete("/clients/delete", deleteAllClients);
+app.delete("/policies/delete", deleteAllPolicies);
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
